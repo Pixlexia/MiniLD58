7dbaf8e4cec7df3223c60868eaac34704072f880
Davy Bolivar <davy.bolivar@gmail.com>
2015-03-23 16:57:47 +0800

    Menu

---
+++ Assets/CursorEffect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorEffect : MonoBehaviour {
+
+	public float maxAngle, minAngle, dur;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Swing ();
+	}
+
+	void Swing(){
+		transform.rotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.y,
+		                                             Mathf.PingPong(Time.time * dur, maxAngle-minAngle)+minAngle);
+	}
+}
